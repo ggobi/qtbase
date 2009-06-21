51d44164c983eb398e0d907aa1f4a7d96ff0aad8
Michael Lawrence <lawrence.michael@gene.com>
2009-06-21 22:59:06 +0000

    Add simple qfont constructor

---
+++ R/font.R
@@ -0,0 +1,11 @@
+qfont <-
+  function(family = "Arial",
+           pointsize = -1L,
+           weight = -1L,
+           italic = FALSE, ...)
+{
+  list(family = family,
+       pointsize = pointsize,
+       weight = weight,
+       italic = italic)
+}
