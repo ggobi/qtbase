b02959ec4247687bbe8217576e196146246310fc
Michael Lawrence <lawrence.michael@gene.com>
2009-06-27 15:45:39 +0000

    Add qtdefs.h public header, which for now defines QT_BEGIN/END_DECLS for declaring C functions in a way compatible with C and C++.

--- inst/include/qtbase.h
+++ inst/include/qtbase.h
@@ -8,6 +8,8 @@
 
 #include <Rinternals.h>
 
+#include <qtdefs.h>
+
 QObject *unwrapQObjectReferee(SEXP x);
 
 #define unwrapPointer(x, type) ((type *) R_ExternalPtrAddr(x))
@@ -26,27 +28,29 @@ QObject *unwrapQObjectReferee(SEXP x);
 
 // FIXME: we should not have duplicate header files inside and outside
 // the package. Could use symbolic links.
-extern "C" {
-  SEXP wrapQObject(QObject *object);
-  SEXP wrapQWidget(QWidget *widget);
-  SEXP wrapPointer(void *ptr, const char *className, R_CFinalizer_t finalizer);
-  SEXP wrapQGraphicsWidget(QGraphicsWidget *widget);
-  SEXP wrapQGraphicsItem(QGraphicsItem *item);
+QT_BEGIN_DECLS
 
-  // Conversion routines
-  // R -> C/Qt
-  const char ** asStringArray(SEXP s_strs);
-  QString sexp2qstring(SEXP s);
-  // C -> R
-  SEXP asRStringArray(const char * const * strs);
-  SEXP qstring2sexp(QString s);
+SEXP wrapQObject(QObject *object);
+SEXP wrapQWidget(QWidget *widget);
+SEXP wrapPointer(void *ptr, const char *className, R_CFinalizer_t finalizer);
+SEXP wrapQGraphicsWidget(QGraphicsWidget *widget);
+SEXP wrapQGraphicsItem(QGraphicsItem *item);
 
-  // Reference counting
-  void addQObjectReference(QObject *referee, QObject *referer);
-  void addQWidgetReference(QWidget *referee, QObject *referer);
-  void addQGraphicsWidgetReference(QGraphicsWidget *referee, QObject *referer);
-  void addQGraphicsItemReference(QGraphicsItem *referee, QObject *referer);
-}
+// Conversion routines
+// R -> C/Qt
+const char ** asStringArray(SEXP s_strs);
+QString sexp2qstring(SEXP s);
+// C -> R
+SEXP asRStringArray(const char * const * strs);
+SEXP qstring2sexp(QString s);
+
+// Reference counting
+void addQObjectReference(QObject *referee, QObject *referer);
+void addQWidgetReference(QWidget *referee, QObject *referer);
+void addQGraphicsWidgetReference(QGraphicsWidget *referee, QObject *referer);
+void addQGraphicsItemReference(QGraphicsItem *referee, QObject *referer);
+
+QT_END_DECLS
 
 
 #endif
