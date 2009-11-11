d32d5e58ff9460251e0b9f6a23c8a82b174ed034
Justin Talbot <justintalbot@gmail.com>
2009-11-11 22:41:27 +0000

    Added additional comments to the slot test code on the underlying problem that was fixed.

--- tests/slots.R
+++ tests/slots.R
@@ -1,5 +1,13 @@
 library(qtbase)
 
+
+# Tests dynamic slots. This should not crash R.
+# marshal_QString used to try to free the pointer to "x" in the anonymous function.
+# However, the value of "x" was a member variable of QLineEdit ("a") and the
+# deallocation failed. This was fixed by adding a flag to Smoke::StackItem
+# indicating if the item was owned by the StackItem and should therefore be freed
+# by marshal_QString or not.
+
 a <- Qt$QLineEdit()
 b <- Qt$QLineEdit()
 qconnect(a, "textChanged", function(x) { b$text <- x })
