58d7dc26b477aff66dde350619391ec927c6e88d
Justin Talbot <justintalbot@gmail.com>
2009-11-30 19:07:24 +0000

    Added test for multiple inheritance crash

--- tests/slots.R
+++ tests/slots.R
@@ -1,20 +1,26 @@
 library(qtbase)
 
 
-# Tests dynamic slots. This should not crash R.
-# marshal_QString used to try to free the pointer to "x" in the anonymous function.
-# However, the value of "x" was a member variable of QLineEdit ("a") and the
-# deallocation failed. This was fixed by adding a flag to Smoke::StackItem
-# indicating if the item was owned by the StackItem and should therefore be freed
-# by marshal_QString or not.
-
+# Tests dynamic slots. Crashed R due to type-handlers.cpp incorrectly releasing memory.
 a <- Qt$QLineEdit()
 b <- Qt$QLineEdit()
 qconnect(a, "textChanged", function(x) { b$text <- x })
 
-b$text <- "1"
+b$text <- "2"
 a$text <- "1"
 if(!identical(a$text, b$text))
 {
 	stop("Encountered error in testing dynamic slots, ", a$text, " != ", b$text)
 }
+
+
+# Tests multiple inheritance. Crashed R due to environment holding wrong pointer.
+gscene <- Qt$QGraphicsScene()
+rtxt <- gscene$addText("some text")
+rtxt
+(rtxt$toPlainText()) ## fine
+
+item.list <- gscene$items()
+item.list[[1]]
+(item.list[[1]]$toPlainText()) ## crash
+
