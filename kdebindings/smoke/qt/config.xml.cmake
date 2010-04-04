45c2c9cb9c0fabcbf5e7f6349df019f512f7c61b
Michael Lawrence <lawrence.michael@gene.com>
2010-04-04 17:34:16 +0000

    Move the qtdefines setting out of confix.xml and onto the command line. The path is no longer valid after installation, in general, so we do not want it in config.xml.

--- kdebindings/smoke/qt/config.xml.cmake
+++ kdebindings/smoke/qt/config.xml.cmake
@@ -2,7 +2,6 @@
     <resolveTypedefs>true</resolveTypedefs>
     <qtMode>true</qtMode>
     <generator>smoke</generator>
-    <definesList>@CMAKE_CURRENT_BINARY_DIR@/../qtdefines</definesList>
     <includeDirs>
         <dir>@QT_INCLUDE_DIR@</dir>
         <dir>@QT_QTCORE_INCLUDE_DIR@</dir>
