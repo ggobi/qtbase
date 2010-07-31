7474e9376de28ce678c976495f7aeb39573c82aa
Michael Lawrence <lawrence.michael@gene.com>
2010-07-31 14:21:13 +0000

    Remove needless default values for qconnect args

--- R/connect.R
+++ R/connect.R
@@ -1,12 +1,14 @@
 
 ## TODO: possibly allow a 'slot' parameter, and only pass the
 ## indicated arguments. 
-qconnect <- function(x, signal, handler = NULL, user.data = NULL)
+qconnect <- function(x, signal, handler, user.data)
 {
   stopifnot(is(x, "QObject"))
   stopifnot(is.function(handler))
   signal <- as.character(signal)
   has.user.data <- !missing(user.data)
+  if (!has.user.data)
+    user.data <- NULL
   nargs <- length(formals(handler))
   if (has.user.data && !nargs)
     stop("if 'user.data' specified, handler must take at least one argument")
