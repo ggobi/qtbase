83ebf083ff78f39244ae7423aa26d30e370a4754
Michael Lawrence <lawremi@gmail.com>
2010-11-19 07:05:29 -0800

    Add the dim methods from qtpaint. Should probably expand these.

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,20 @@
+## Holds some convenience wrappers for various objects
 
-## Utility functions moved over from the formerly qtgui package
-
+##' Get the dimensions of rectangles and rectangular objects
+##'
+##' @param x A rectangular object, like \code{QRect} or \code{QGraphicsItem}
+##' @method dim QRectF
+##' @rdname dim-methods
+dim.QRectF <- function(x) c(x$width(), x$height())
+##' @method dim QRect
+##' @rdname dim-methods
+dim.QRect <- dim.QRectF
+##' @method dim QGraphicsScene
+##' @rdname dim-methods
+dim.QGraphicsScene <- function(x) dim(x$sceneRect)
+##' @method dim QGraphicsItem
+##' @rdname dim-methods
+dim.QGraphicsItem <- function(x) dim(x$boundingRect)
+##' @method dim QGraphicsView
+##' @rdname dim-methods
+dim.QGraphicsView <- function(x) dim(x$viewport()$rect)
