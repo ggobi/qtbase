4164cfe96910fc6757e208cfe6e3d0b911a05717
Michael Lawrence <michafla@gene.com>
2012-06-15 06:10:37 -0700

    DEF file generation is now robust to 64 bit addresses.

--- src/mkdef.sh
+++ src/mkdef.sh
@@ -1,5 +1,5 @@
 echo EXPORTS > qtbase.def
 nm -g --defined-only CMakeFiles/qtbase.dir/*.obj > tmp
-sed -n '/^........ [T|C|B] _/s/^........ [T|C|B] _/ /p' tmp >> qtbase.def
+sed -n '/^[^ ]* [T|C|B] /s/^[^ ]* [T|C|B] / /p' tmp >> qtbase.def
 rm tmp
 
