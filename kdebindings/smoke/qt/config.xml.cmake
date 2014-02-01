a4cf45a590ef0af5a0a638412b9cf28408f65344
Michael Lawrence <michafla@gene.com>
2014-02-01 05:18:55 -0800

    drop obsolete comment about the include dirs not working on the Mac

--- kdebindings/smoke/qt/config.xml.cmake
+++ kdebindings/smoke/qt/config.xml.cmake
@@ -3,12 +3,6 @@
     <qtMode>true</qtMode>
     <generator>smoke</generator>
     <includeDirs>
-<!-- NOTE: These paths are essentially useless on the Mac, because the
-     Qt installer sticks everything under the default framework
-     directory where our hacked generator knows to look.  The ideal
-     solution would be to have a <frameworkDirs> element in this file,
-     which would specify @QT_INCLUDE_DIR@ as a directory element. This
-     is awaiting a use-case.  -->
 @SMOKE_CONFIG_INCLUDE_DIR_ELEMENTS@
     </includeDirs>
 </config>
